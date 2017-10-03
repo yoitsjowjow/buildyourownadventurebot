8037ed0b77195d48487859149386e73cfdd49048
Rich Hildred <rhildred@ip-172-31-5-173.us-west-2.compute.internal>
2017-10-03 10:57:53 -0400

    got it receiving a twillio message

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var bodyParser = require('body-parser');
 
 // Define the port to run on
 app.set('port', 5100);
@@ -9,11 +10,22 @@ app.set('port', 5100);
 var sPath = path.join(__dirname, '.');
 
 app.use(express.static(sPath));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define a method for the webhook
 app.get("/api/chatbot", (req, res)=>{ 
     res.send("Hello from the chatbot");
 
+});
+
+//define a method for the twilio webhook
+app.post('/sms', function(req, res) {
+  console.log(req.body);
+  var twilio = require('twilio');
+  var twiml = new twilio.twiml.MessagingResponse();
+  twiml.message('The Robots are coming! Head for the hills!');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
 });
 
 // Listen for requests
